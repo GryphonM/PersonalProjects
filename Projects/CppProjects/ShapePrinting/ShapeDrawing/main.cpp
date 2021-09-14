144a99b1f1d36c5e23fc5f57c6b10d6b983836c7
Gryphon M <gryphon.mclaughlin@digipen.edu>
2021-09-14 13:15:16 -0700

    Filled Rectangle Working

--- Projects/CppProjects/ShapePrinting/ShapeDrawing/main.cpp
+++ Projects/CppProjects/ShapePrinting/ShapeDrawing/main.cpp
@@ -9,31 +9,59 @@
 //
 //------------------------------------------------------------------------------
 
+//include files
 #include <iostream>
 #include <iomanip>
 
+//function prototypes
+void PrintRectangle(bool filled);
+void PrintTriangle(bool filled);
+
+//using statements
+using std::cout;
+using std::cin;
+
 int main(void)
 {
-	using namespace std;
-	
 	int type;
+	bool exit = false;
 
-	//ask user what kind of shape they want
-	cout << "What would you like to draw?" << endl;
-	cout << "1 - A Filled Rectangle" << endl;
-	cout << "2 - A Non Filled Rectangle" << endl;
-	cout << "3 - A Filled Triangle" << endl;
-	cout << "4 - A Non Filled Triangle" << endl;
-	cout << "\nYour choice is: ";
-	cin >> type;
-
-	//call appropriate function based on type selected
-	switch (type)
+	while (!exit)
 	{
+		//ask user what kind of shape they want
+		cout << "What would you like to draw?" << std::endl;
+		cout << "1 - A Filled Rectangle" << std::endl;
+		cout << "2 - A Non Filled Rectangle" << std::endl;
+		cout << "3 - A Filled Triangle" << std::endl;
+		cout << "4 - A Non Filled Triangle" << std::endl;
+		cout << "Any Other Number - Exit" << std::endl;
+		cout << "\nYour choice is: ";
+		cin >> type;
+
+		//call appropriate function based on type selected
+		switch (type)
+		{
 		case 1:
 			PrintRectangle(true);
+			cout << std::endl;
+			break;
 		case 2:
 			PrintRectangle(false);
+			cout << std::endl;
+			break;
+		case 3:
+			PrintTriangle(true);
+			cout << std::endl;
+			break;
+		case 4:
+			PrintTriangle(false);
+			cout << std::endl;
+			break;
+		default:
+			exit = true;
+			cout << "Thank You for using our application!" << std::endl;
+			break;
+		}
 	}
 }
 
@@ -42,4 +70,36 @@ void PrintRectangle(bool filled)
 	int x, y;
 	cout << "Enter the rectangle's width and height values: ";
 	cin >> x >> y;
+
+	if (filled)
+	{
+		for (int i = 0; i < y; i++)
+		{
+			for (int j = 0; j < x; j++)
+			{
+				cout << "*";
+			}
+			cout << std::endl;
+		}
+	}
+	else
+	{
+
+	}
+}
+
+void PrintTriangle(bool filled)
+{
+	int x;
+	std::cout << "Enter the triangle's height: ";
+	std::cin >> x;
+
+	if (filled)
+	{
+
+	}
+	else
+	{
+
+	}
 }
