0a77e48836835e7705c6caa11394198ef986f31a
Gryphon M <gryphon.mclaughlin@digipen.edu>
2021-09-20 14:39:01 -0700

    Namespace Updates

--- Projects/CppProjects/Tic-Tac-Toe/board.cpp
+++ Projects/CppProjects/Tic-Tac-Toe/board.cpp
@@ -14,75 +14,73 @@
 //------------------------------------------------------------------------------
 #include "Board.h"
 
-//------------------------------------------------------------------------------
-// Namespace Declarations:
-//------------------------------------------------------------------------------
-using namespace CS170;
-
-//------------------------------------------------------------------------------
-// Struct Definitions:
-//------------------------------------------------------------------------------
-
-struct Board
+namespace CS170
 {
-	TileState** data;
-};
+	//------------------------------------------------------------------------------
+	// Struct Definitions:
+	//------------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------
-// Function Declarations:
-//------------------------------------------------------------------------------
+	struct Board
+	{
+		TileState** data;
+	};
 
-// Allocate memory for a tic-tac-toe board.
+	//------------------------------------------------------------------------------
+	// Function Declarations:
+	//------------------------------------------------------------------------------
+
+	// Allocate memory for a tic-tac-toe board.
+		// Returns:
+		//   A pointer to the created board struct.
+	Board* BoardCreate()
+	{
+		Board *newBoard = new Board;
+	}
+
+	// Free memory for the tic-tac-toe board.
+	// Params:
+	//   theBoard = A reference to the pointer to the board.
+	void BoardFree(Board*& theBoard)
+	{
+
+	}
+
+	// Display the contents of the board using the standard output stream.
+	// Params:
+	//   theBoard = A reference to the game board.
+	void BoardDisplay(const Board& board)
+	{
+
+	}
+
+	// Place a token on the board at a specific position.
+	// Params:
+	//   theBoard = A reference to the game board.
+	//   row = The row of the board on which to place the token.
+	//   column = The column of the board on which to place the token.
+	//   value = The value to place in the specified tile.
 	// Returns:
-	//   A pointer to the created board struct.
-Board* BoardCreate()
-{
+	//   Whether the token was able to be placed.
+	PlaceResult BoardPlaceToken(Board& board, unsigned row, unsigned column, TileState value)
+	{
 
-}
+	}
 
-// Free memory for the tic-tac-toe board.
-// Params:
-//   theBoard = A reference to the pointer to the board.
-void BoardFree(Board*& theBoard)
-{
+	// Reset the board to an empty state.
+	// Params:
+	//   theBoard = A reference to the game board.
+	void BoardReset(Board& board)
+	{
 
-}
+	}
 
-// Display the contents of the board using the standard output stream.
-// Params:
-//   theBoard = A reference to the game board.
-void BoardDisplay(const Board& board)
-{
+	// Get the current state of the board. (Is the game over?)
+	// Params:
+	//   theBoard = A reference to the game board.
+	// Returns:
+	//   The current state of the game - win, tie, or open (still going).
+	BoardState BoardGetState(const Board& board)
+	{
 
-}
-
-// Place a token on the board at a specific position.
-// Params:
-//   theBoard = A reference to the game board.
-//   row = The row of the board on which to place the token.
-//   column = The column of the board on which to place the token.
-//   value = The value to place in the specified tile.
-// Returns:
-//   Whether the token was able to be placed.
-PlaceResult BoardPlaceToken(Board& board, unsigned row, unsigned column, TileState value)
-{
-
-}
-
-// Reset the board to an empty state.
-// Params:
-//   theBoard = A reference to the game board.
-void BoardReset(Board& board)
-{
-
-}
-
-// Get the current state of the board. (Is the game over?)
-// Params:
-//   theBoard = A reference to the game board.
-// Returns:
-//   The current state of the game - win, tie, or open (still going).
-BoardState BoardGetState(const Board& board)
-{
-
+	}
 }
