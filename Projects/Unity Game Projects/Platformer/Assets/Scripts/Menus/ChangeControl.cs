9183610ef75f9dfddb4a306bb67aa9099935e750
Gryphon M <gryphon.mclaughlin@digipen.edu>
2021-10-28 15:01:41 -0700

    Button Text Corrections

--- Projects/Unity Game Projects/Platformer/Assets/Scripts/Menus/ChangeControl.cs
+++ Projects/Unity Game Projects/Platformer/Assets/Scripts/Menus/ChangeControl.cs
@@ -36,6 +36,122 @@ public class ChangeControl : MonoBehaviour
                 if (Input.GetKeyDown(key))
                 {
                     SetKey(key);
+
+                    switch (key)
+                    {
+                        case KeyCode.Keypad0:
+                        case KeyCode.Alpha0:
+                            buttonText.text = "0";
+                            break;
+                        case KeyCode.Keypad1:
+                        case KeyCode.Alpha1:
+                            buttonText.text = "1";
+                            break;
+                        case KeyCode.Keypad2:
+                        case KeyCode.Alpha2:
+                            buttonText.text = "2";
+                            break;
+                        case KeyCode.Keypad3:
+                        case KeyCode.Alpha3:
+                            buttonText.text = "3";
+                            break;
+                        case KeyCode.Keypad4:
+                        case KeyCode.Alpha4:
+                            buttonText.text = "4";
+                            break;
+                        case KeyCode.Keypad5:
+                        case KeyCode.Alpha5:
+                            buttonText.text = "5";
+                            break;
+                        case KeyCode.Keypad6:
+                        case KeyCode.Alpha6:
+                            buttonText.text = "6";
+                            break;
+                        case KeyCode.Keypad7:
+                        case KeyCode.Alpha7:
+                            buttonText.text = "7";
+                            break;
+                        case KeyCode.Keypad8:
+                        case KeyCode.Alpha8:
+                            buttonText.text = "8";
+                            break;
+                        case KeyCode.Keypad9:
+                        case KeyCode.Alpha9:
+                            buttonText.text = "9";
+                            break;
+                        case KeyCode.KeypadPeriod:
+                            buttonText.text = ".";
+                            break;
+                        case KeyCode.KeypadDivide:
+                            buttonText.text = "/";
+                            break;
+                        case KeyCode.KeypadMultiply:
+                        case KeyCode.Asterisk:
+                            buttonText.text = "*";
+                            break;
+                        case KeyCode.KeypadMinus:
+                        case KeyCode.Minus:
+                            buttonText.text = "-";
+                            break;
+                        case KeyCode.KeypadPlus:
+                        case KeyCode.Plus:
+                            buttonText.text = "+";
+                            break;
+                        case KeyCode.KeypadEnter:
+                            buttonText.text = "Return";
+                            break;
+                        case KeyCode.KeypadEquals:
+                            buttonText.text = "=";
+                            break;
+                        case KeyCode.UpArrow:
+                            buttonText.text = "Up Arrow";
+                            break;
+                        case KeyCode.DownArrow:
+                            buttonText.text = "Down Arrow";
+                            break;
+                        case KeyCode.RightArrow:
+                            buttonText.text = "Right Arrow";
+                            break;
+                        case KeyCode.LeftArrow:
+                            buttonText.text = "Left Arrow";
+                            break;
+                        case KeyCode.PageUp:
+                            buttonText.text = "Page Up";
+                            break;
+                        case KeyCode.PageDown:
+                            buttonText.text = "Page Down";
+                            break;
+                        case KeyCode.Exclaim:
+                            buttonText.text = "!";
+                            break;
+                        case KeyCode.DoubleQuote:
+                            buttonText.text = "\"";
+                            break;
+                        case KeyCode.Hash:
+                            buttonText.text = "#";
+                            break;
+                        case KeyCode.Dollar:
+                            buttonText.text = "$";
+                            break;
+                        case KeyCode.Percent:
+                            buttonText.text = "%";
+                            break;
+                        case KeyCode.Ampersand:
+                            buttonText.text = "&";
+                            break;
+                        case KeyCode.Quote:
+                            buttonText.text = "'";
+                            break;
+                        case KeyCode.LeftParen:
+                            buttonText.text = "(";
+                            break;
+                        case KeyCode.RightParen:
+                            buttonText.text = ")";
+                            break;
+                        case KeyCode.Comma:
+                            buttonText.text = ",";
+                            break;
+                    }
                     if (key == KeyCode.Mouse0)
                         buttonText.text = "Left Mouse Button";
                     else if (key == KeyCode.Mouse1)
