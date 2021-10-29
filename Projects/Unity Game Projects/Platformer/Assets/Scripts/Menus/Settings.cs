097d298a4c71bf0194dcbe274c799b78bd8927e5
Gryphon M <gryphon.mclaughlin@digipen.edu>
2021-10-29 14:53:14 -0700

    Settings Menu Prefab

--- Projects/Unity Game Projects/Platformer/Assets/Scripts/Menus/Settings.cs
+++ Projects/Unity Game Projects/Platformer/Assets/Scripts/Menus/Settings.cs
@@ -6,18 +6,6 @@ public class Settings : MonoBehaviour
 {
     [SerializeField] GameObject settingsMenu;
     [SerializeField] GameObject mainMenu;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void ActivateMenu(GameObject menu)
     {
