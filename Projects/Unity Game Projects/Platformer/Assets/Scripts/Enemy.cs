96fe04dd073be9b0cc7ad9996701101525f8fea8
Gryphon M <gryphon.mclaughlin@digipen.edu>
2021-11-09 13:55:17 -0800

    Continued Enemy Work
    Currently working, minor bug fixes

--- Projects/Unity Game Projects/Platformer/Assets/Scripts/Enemy.cs
+++ Projects/Unity Game Projects/Platformer/Assets/Scripts/Enemy.cs
@@ -14,10 +14,18 @@ public class Enemy : MonoBehaviour
     [SerializeField] float patrolSpeed = 5f;
     [SerializeField] bool canFly = false;
     [Space(20)]
+    [Tooltip("Tags the enemy can attack\n" +
+        "These game objects must have PlayerHealth.cs or Health.cs attached\n" +
+        "Script will prioritize Health.cs if collision has both scripts")]
+    [SerializeField] string[] Tags;
     [SerializeField] float lungeSpeed = 15f;
     [SerializeField] float lungeDistance = 3f;
-    [SerializeField] float lungeWait = 2f;
+    [SerializeField] float lungePause = 2f;
+    [SerializeField] int damage = 1;
+    [Tooltip("The speed the enemy stops lunging at")]
+    [SerializeField] float lungeEndSpeed = .5f;
     [SerializeField] Vector2 lungeKnockBack;
+    [SerializeField] float timeBetweenLunges;
 
     bool facingRight = true;
     SpriteRenderer mySR;
@@ -28,6 +36,8 @@ public class Enemy : MonoBehaviour
 
     bool waitingToLunge = false;
     bool lunging = false;
+    bool canLunge = true;
+    float lungePauseTimer;
     float lungeWaitTimer;
     
     // Start is called before the first frame update
@@ -36,16 +46,21 @@ public class Enemy : MonoBehaviour
         maxPatrolPoints = patrolPoints.Length - 1;
         mySR = GetComponent<SpriteRenderer>();
         myRB = GetComponent<Rigidbody2D>();
-        lungeWaitTimer = lungeWait;
+        lungePauseTimer = lungePause;
+        lungeWaitTimer = timeBetweenLunges;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 targetDir = new Vector2(transform.position.x - target.transform.position.x, 
-            transform.position.y - target.transform.position.y);
+        Vector2 targetDir = new Vector2(target.transform.position.x - transform.position.x, 
+            target.transform.position.y - transform.position.y);
+
+        bool targetClose = targetDir.magnitude < lungeDistance &&
+            ((facingRight && target.transform.position.x > transform.position.x) || 
+            (!facingRight && target.transform.position.x < transform.position.x));
         
-        if (targetDir.magnitude > lungeDistance && !waitingToLunge && !lunging)
+        if (!targetClose && !waitingToLunge && !lunging)
         {
             if ((transform.position.x <= patrolPoints[nextPatrolPoint].x + patrolCloseEnough && 
                 transform.position.x >= patrolPoints[nextPatrolPoint].x - patrolCloseEnough) &&
@@ -53,12 +68,18 @@ public class Enemy : MonoBehaviour
                 (transform.position.y <= patrolPoints[nextPatrolPoint].y + patrolCloseEnough &&
                 transform.position.y >= patrolPoints[nextPatrolPoint].y - patrolCloseEnough)))
             {
-                if (nextPatrolPoint == maxPatrolPoints || (!resetToStart && nextPatrolPoint == 0))
+                if (nextPatrolPoint == maxPatrolPoints || (!resetToStart && nextPatrolPoint == 0 && !countingUp))
                 {
                     if (resetToStart)
                         nextPatrolPoint = 0;
                     else
+                    {
                         countingUp = !countingUp;
+                        if (countingUp)
+                        nextPatrolPoint++;
+                    else
+                        nextPatrolPoint--;
+                    }
                 }
                 else
                 {
@@ -79,8 +100,8 @@ public class Enemy : MonoBehaviour
             }
             else
             {
-                Vector2 newVel = new Vector2(transform.position.x - patrolPoints[nextPatrolPoint].x,
-                    transform.position.y - patrolPoints[nextPatrolPoint].y);
+                Vector2 newVel = new Vector2(patrolPoints[nextPatrolPoint].x - transform.position.x,
+                    patrolPoints[nextPatrolPoint].y - transform.position.y);
 
                 newVel.Normalize();
 
@@ -99,20 +120,20 @@ public class Enemy : MonoBehaviour
         }
         else
         {
-            if (!waitingToLunge && !lunging)
+            if (!waitingToLunge && !lunging && canLunge)
             {
                 waitingToLunge = true;
                 myRB.velocity = Vector2.zero;
-                myRB.bodyType = RigidbodyType2D.Kinematic;
+                //myRB.bodyType = RigidbodyType2D.Kinematic;
             }
             
-            if (waitingToLunge && !lunging)
+            if (waitingToLunge && !lunging && canLunge)
             {
-                if (lungeWaitTimer <= 0)
+                if (lungePauseTimer <= 0)
                 {
                     waitingToLunge = false;
                     lunging = true;
-                    lungeWaitTimer = lungeWait;
+                    lungePauseTimer = lungePause;
 
                     Vector2 newVel = targetDir.normalized;
                     if (canFly)
@@ -124,17 +145,72 @@ public class Enemy : MonoBehaviour
                         newVel.x *= lungeSpeed;
                         newVel.y = 0;
                     }
+                    myRB.velocity = newVel;
+                }
+                else
+                    lungePauseTimer -= Time.deltaTime;
+            }
+
+            if (lunging && canLunge)
+            {
+                if (myRB.velocity.x > lungeEndSpeed || (canFly && myRB.velocity.y > lungeEndSpeed))
+                {
+                    if (canFly)
+                    {
+                        Vector2 newVel = myRB.velocity.normalized;
+                        newVel *= myRB.velocity.magnitude - Time.deltaTime;
+                        myRB.velocity = newVel;
+                    }
+                    else
+                    {
+                        Vector2 newVel = myRB.velocity;
+                        newVel.x -= Time.deltaTime;
+                        myRB.velocity = newVel;
+                    }
+                }
+                else
+                {
+                    canLunge = false;
+                    lunging = true;
+                    myRB.velocity = Vector2.zero;
+                }
+            }
+
+            if (!canLunge)
+            {
+                if (lungeWaitTimer <= 0)
+                {
+                    canLunge = true;
+                    lungeWaitTimer = timeBetweenLunges;
                 }
                 else
                     lungeWaitTimer -= Time.deltaTime;
             }
-
-            if (lunging)
-            {
-                // TO DO: Decrease lunge velocity
-            }
         }
     }
 
-    // TO DO: End Lunge Behaviour
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        foreach(string tag in Tags)
+        {
+            if (collision.gameObject.CompareTag(tag))
+            {
+                Health colHealth = collision.gameObject.GetComponent<Health>();
+                PlayerHealth colPlayHealth = collision.gameObject.GetComponent<PlayerHealth>();
+
+                if (colHealth != null)
+                    colHealth.DamageObject(damage);
+                else if (colPlayHealth != null)
+                    colPlayHealth.DamagePlayer(damage);
+
+                if (lunging)
+                {
+                    if (facingRight)
+                        myRB.velocity = -lungeKnockBack;
+                    else
+                        myRB.velocity = lungeKnockBack;
+                }
+            }
+        }
+    }
 }
