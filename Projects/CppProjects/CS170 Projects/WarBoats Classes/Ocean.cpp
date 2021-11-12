c34d5444dd8945d6561014e5a693dfc9851f359d
Gryphon M <gryphon.mclaughlin@digipen.edu>
2021-11-12 15:00:36 -0800

    Begin War Boats Classes Work

--- Projects/CppProjects/CS170 Projects/WarBoats Classes/Ocean.cpp
+++ Projects/CppProjects/CS170 Projects/WarBoats Classes/Ocean.cpp
@@ -5,11 +5,44 @@ namespace CS175
 {
     namespace WarBoats
     {  
-        /*
-        * ALL STUDENT IMPLEMENTATION GOES HERE
-        */
+        Ocean::Ocean(int num_boats_, int x_quadrants_, int y_quadrants_)
+            : x_quadrants(x_quadrants_), y_quadrants(y_quadrants_)
+        {
+            // Set Up Grid
+            grid = new int* [x_quadrants];
+            for (int i = 0; i < x_quadrants; i++)
+            {
+                grid[i] = new int[y_quadrants];
 
-        const int *Ocean::GetGrid(void) const
+                // Initialize values
+                for (int j = 0; j < y_quadrants; j++)
+                    grid[i][j] = 0;
+            }
+
+            // Set Up Boats
+            boats = new int[num_boats_];
+            // Initialize values
+            for (int i = 0; i < num_boats_; i++)
+                boats[i] = 0;
+        }
+
+        Ocean::~Ocean(void)
+        {
+            // Delete Grid
+            for (int i = 0; i < x_quadrants; i++)
+                delete[] grid[i];
+            delete[] grid;
+
+            // Delete Boats Array
+            delete[] boats;
+        }
+
+        BoatPlacement Ocean::PlaceBoat(void)
+        {
+
+        }
+
+        const int **Ocean::GetGrid(void) const
         {
             return grid;
         }
--- Projects/CppProjects/CS170 Projects/WarBoats Classes/Ocean.h
+++ Projects/CppProjects/CS170 Projects/WarBoats Classes/Ocean.h
@@ -13,16 +13,17 @@ namespace CS175
     {
       public:
         
-          /*
-           * Other public methods
-          */
+          Ocean(int num_boats_, int x_quadrants_, int y_quadrants_);
+          ~Ocean(void);
+          BoatPlacement PlaceBoat(void);
         
            // Provided
-        const int *GetGrid(void) const;
+        const int **GetGrid(void) const;
         Point GetDimensions(void) const;
 
       private:
-        int *grid;        // The 2D ocean
+        int **grid;        // The 2D ocean
+        int* boats;       // How many hits each boat has taken
         int x_quadrants;  // Ocean size along x-axis
         int y_quadrants;  // Ocean size along y-axis
         
