23ae4e5094fd9511fa909d15a0fce5f7540c9be6
Gryphon M <gryphon.mclaughlin@digipen.edu>
2021-11-30 14:09:11 -0800

    Documentation

--- Projects/CppProjects/Vector2D/Vector2D.cpp
+++ Projects/CppProjects/Vector2D/Vector2D.cpp
@@ -1,3 +1,14 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	Vector2D.cpp
+// Author(s):	Gryphon McLaughlin (gryphon.mclaughlin)
+// Project:	PRG 6.3.6 ASSIGNMENT - Vector2D Class
+// Course:	WANIC VGP2
+//
+// Copyright © 2019 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
 #include "Vector2D.h"  // Vector2D members
 #include <cmath>    // sqrt, atan, sin, cos
 
@@ -8,11 +19,20 @@ namespace CS170
 // 22 public member functions (2 constructors, 2 accessors, 2 mutators, 7 others, 9 operators) 
 
 // Constructors (2)
+// Builds a Vector2D with the given values
+// Params:
+//   x = Value for X.
+//   y = Value for Y.
+// Return:
+//   This Vector2D Object
 Vector2D::Vector2D(float x_, float y_)
   : x(x_), y(y_)
 {
 }
 
+// Builds a Vector2D with default values
+// Return:
+//   This Vector2D Object
 Vector2D::Vector2D()
 {
     x = 0;
@@ -20,11 +40,13 @@ Vector2D::Vector2D()
 }
 
 // Accessors (2)
+// Returns the value of x.
 float Vector2D::X() const
 {
     return x;
 }
 
+// Returns the value of x.
 float Vector2D::Y() const
 {
     return y;
@@ -32,11 +54,17 @@ float Vector2D::Y() const
 
 
 // Mutators (2)
+// Changes the value of x to the given value
+// Params:
+//   newVal = The new value for X.
 void Vector2D::X(const float newVal)
 {
     x = newVal;
 }
 
+// Changes the value of y to the given value
+// Params:
+//   newVal = The new value for Y.
 void Vector2D::Y(const float newVal)
 {
     y = newVal;
@@ -44,81 +72,145 @@ void Vector2D::Y(const float newVal)
 
 
 // Other functions (7)
+// Evaluates the squared magnitude of the vector.
+// Return:
+//   The squared Magnitude in a float
 float Vector2D::MagnitudeSquared() const
 {
     return (x * x) + (y * y);
 }
 
+// Evaluates the magnitude of the vector.
+// Return:
+//   The Magnitude in a float
 float Vector2D::Magnitude() const
 {
     return sqrt(MagnitudeSquared());
 }
 
+// Evaluates the normalized value of the vector.
+// Return:
+//   The normalized values
 Vector2D Vector2D::Normalized() const
 {
     return *this * (1 / Magnitude());
 }
 
+// Determines the squared distance between this vector and another vector
+// Params:
+//   other = The other vector to determine the squared distance between.
+// Return:
+//   The squared distance in a float
 float Vector2D::DistanceSquared(const Vector2D& other) const
 {
     Vector2D delta = *this - other;
 
-    return (delta.X() * delta.X()) + (delta.Y() * delta.Y());
+    return (delta.x * delta.x) + (delta.y * delta.y);
 }
 
+// Determines distance between this vector and another vector
+// Params:
+//   other = The other vector to determine the distance between.
+// Return:
+//   The distance in a float
 float Vector2D::Distance(const Vector2D& other) const
 {
     return sqrt(DistanceSquared(other));
 }
 
+// Determines midpoint between two Vectors
+// Params:
+//   other = The other vector to determine the midpoint between.
+// Return:
+//   The mipoint in a Vector
 Vector2D Vector2D::Midpoint(const Vector2D& other) const
 {
     return 0.5 * (*this + other);
 }
 
+// Determines the dot product of this vector and a given vector
+// Params:
+//   other = The other vector to determine the midpoint between.
+// Return:
+//   The dot product in a float
 float Vector2D::DotProduct(const Vector2D& other) const
 {
-    return (x * other.X()) + (y * other.Y());
+    return (x * other.x) + (y * other.y);
 }
 
 
 // Operators (9)
+// Overloads the + operator to add two vectors
+// Params:
+//   other = The other vector to add.
+// Return:
+//   A vector with the added values
 Vector2D Vector2D::operator+(const Vector2D& other) const
 {
-    return Vector2D(x + other.X(), y + other.Y());
+    return Vector2D(x + other.x, y + other.y);
 }
 
+// Overloads the - operator to subtract two vectors
+// Params:
+//   other = The other vector to subtract.
+// Return:
+//   A vector with the subtracted values
 Vector2D Vector2D::operator-(const Vector2D& other) const
 {
-    return Vector2D(x - other.X(), y - other.Y());
+    return Vector2D(x - other.x, y - other.y);
 }
 
+// Overloads the * operator to mulitply this value by a scalar
+// Params:
+//   other = The scalar.
+// Return:
+//   A vector with the scaled values
 Vector2D Vector2D::operator*(const float other) const
 {
     return Vector2D(x * other, y * other);
 }
 
+// Overloads the / operator to divide this vector by a number
+// Params:
+//   other = The value to divide the vector by.
+// Return:
+//   A vector with the divided values
 Vector2D Vector2D::operator/(const float other) const
 {
     return Vector2D(x / other, y / other);
 }
 
+// Overloads the += operator to add two vectors and assign the result to this vector
+// Params:
+//   other = The other vector to add.
+// Return:
+//   A pointer to this vector with the changed values
 Vector2D& Vector2D::operator+=(const Vector2D& other)
 {
-    x += other.X();
-    y += other.Y();
+    x += other.x;
+    y += other.y;
 
     return *this;
 }
 
+// Overloads the -= operator to subtract two vectors and assign the result to this vector
+// Params:
+//   other = The other vector to subtract.
+// Return:
+//   A pointer to this vector with the changed values
 Vector2D& Vector2D::operator-=(const Vector2D& other)
 {
-    x -= other.X();
-    y -= other.Y();
+    x -= other.x;
+    y -= other.y;
 
     return *this;
 }
 
+// Overloads the *= operator to multiply this vector by a scalar and assign it to this vector
+// Params:
+//   other = The scalar.
+// Return:
+//   A pointer to this vector with the changed values
 Vector2D& Vector2D::operator*=(const float other)
 {
     x *= other;
@@ -127,6 +219,11 @@ Vector2D& Vector2D::operator*=(const float other)
     return *this;
 }
 
+// Overloads the /= operator to divide this vector by a number and assign it to this vector
+// Params:
+//   other = The number to divide this vector by.
+// Return:
+//   A pointer to this vector with the changed values
 Vector2D& Vector2D::operator/=(const float other)
 {
     x /= other;
@@ -135,6 +232,9 @@ Vector2D& Vector2D::operator/=(const float other)
     return *this;
 }
 
+// Overloads the - operator to convert this vector to the negative.
+// Return:
+//   A Vector with the negated values
 Vector2D Vector2D::operator-() const
 {
     return Vector2D(-x, -y);
@@ -143,6 +243,12 @@ Vector2D Vector2D::operator-() const
 
 ///////////////////////////////////////////////////////////////////////////////
 // 2 non-member, non-friend functions (operators)
+// Overloads the << operator to print out the vector
+// Params:
+//   os = the stream to output the output to.
+//   output = the vector to be printed.
+// Return:
+//   the stream with the value to be printed
 std::ostream& operator<<(std::ostream& os, const Vector2D& out)
 {
     os << "(" << out.X() << ", ";
@@ -151,6 +257,12 @@ std::ostream& operator<<(std::ostream& os, const Vector2D& out)
     return os;
 }
 
+// Overloads the * operator to multiply a float by a vector
+// Params:
+//   other = the scalar.
+//   vector2D = the vector to be scaled.
+// Return:
+//   The result of the inverse operation (i.e. Vector2D * other).
 Vector2D operator*(float other, const Vector2D& vector2D)
 {
     return vector2D * other;
--- Projects/CppProjects/Vector2D/Vector2D.h
+++ Projects/CppProjects/Vector2D/Vector2D.h
@@ -1,3 +1,14 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	Vector2D.h
+// Author(s):	Gryphon McLaughlin (gryphon.mclaughlin)
+// Project:	PRG 6.3.6 ASSIGNMENT - Vector2D Class
+// Course:	WANIC VGP2
+//
+// Copyright © 2019 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
 ////////////////////////////////////////////////////////////////////////////////
 #ifndef VECTOR2D_H
 #define VECTOR2D_H
@@ -11,37 +22,140 @@ namespace CS170
 	{
 	public:
 		// Constructors (2)
+		// Builds a Vector2D with the given values
+		// Params:
+		//   x = Value for X.
+		//   y = Value for Y.
+		// Return:
+		//   This Vector2D Object
 		Vector2D(float x, float y);
+		// Builds a Vector2D with default values
+		// Return:
+		//   This Vector2D Object
 		Vector2D();
 		
 		// Accessors (2)
+		// Returns the value of x.
 		float X() const;
+		// Returns the value of x.
 		float Y() const;
 		
 		
 		// Mutators (2)
+		// Changes the value of x to the given value
+		// Params:
+		//   newVal = The new value for X.
 		void X(const float newVal);
+		// Changes the value of y to the given value
+		// Params:
+		//   newVal = The new value for Y.
 		void Y(const float newVal);
 		
 		
 		// Other functions (7)
+		// Evaluates the squared magnitude of the vector.
+		// Return:
+		//   The squared Magnitude in a float
 		float MagnitudeSquared() const;
+
+		// Evaluates the magnitude of the vector.
+		// Return:
+		//   The Magnitude in a float
 		float Magnitude() const;
+
+		// Evaluates the normalized value of the vector.
+		// Return:
+		//   The normalized values
 		Vector2D Normalized() const;
+
+		// Determines the squared distance between this vector and another vector
+		// Params:
+		//   other = The other vector to determine the squared distance between.
+		// Return:
+		//   The squared distance in a float
 		float DistanceSquared(const Vector2D& other) const;
+
+		// Determines distance between this vector and another vector
+		// Params:
+		//   other = The other vector to determine the distance between.
+		// Return:
+		//   The distance in a float
 		float Distance(const Vector2D& other) const;
+
+		// Determines midpoint between two Vectors
+		// Params:
+		//   other = The other vector to determine the midpoint between.
+		// Return:
+		//   The mipoint in a Vector
 		Vector2D Midpoint(const Vector2D& other) const;
+
+		// Determines the dot product of this vector and a given vector
+		// Params:
+		//   other = The other vector to determine the midpoint between.
+		// Return:
+		//   The dot product in a float
 		float DotProduct(const Vector2D& other) const;
 
 		// Overloaded operators (9 member functions)
+		// Overloads the + operator to add two vectors
+		// Params:
+		//   other = The other vector to add.
+		// Return:
+		//   A vector with the added values
 		Vector2D operator+(const Vector2D& other) const;
+
+		// Overloads the - operator to subtract two vectors
+		// Params:
+		//   other = The other vector to subtract.
+		// Return:
+		//   A vector with the subtracted values
 		Vector2D operator-(const Vector2D& other) const;
+
+		// Overloads the * operator to mulitply this value by a scalar
+		// Params:
+		//   other = The scalar.
+		// Return:
+		//   A vector with the scaled values
 		Vector2D operator*(const float other) const;
+
+		// Overloads the / operator to divide this vector by a number
+		// Params:
+		//   other = The value to divide the vector by.
+		// Return:
+		//   A vector with the divided values
 		Vector2D operator/(const float other) const;
+
+		// Overloads the += operator to add two vectors and assign the result to this vector
+		// Params:
+		//   other = The other vector to add.
+		// Return:
+		//   A pointer to this vector with the changed values
 		Vector2D& operator+=(const Vector2D& other);
+
+		// Overloads the -= operator to subtract two vectors and assign the result to this vector
+		// Params:
+		//   other = The other vector to subtract.
+		// Return:
+		//   A pointer to this vector with the changed values
 		Vector2D& operator-=(const Vector2D& other);
+
+		// Overloads the *= operator to multiply this vector by a scalar and assign it to this vector
+		// Params:
+		//   other = The scalar.
+		// Return:
+		//   A pointer to this vector with the changed values
 		Vector2D& operator*=(const float other);
+
+		// Overloads the /= operator to divide this vector by a number and assign it to this vector
+		// Params:
+		//   other = The number to divide this vector by.
+		// Return:
+		//   A pointer to this vector with the changed values
 		Vector2D& operator/=(const float other);
+
+		// Overloads the - operator to convert this vector to the negative.
+		// Return:
+		//   A Vector with the negated values
 		Vector2D operator-() const;
 		
 		
@@ -51,7 +165,20 @@ namespace CS170
 	};
   
 	// Overloaded operators (2 non-member, non-friend functions)
+	// Overloads the << operator to print out the vector
+	// Params:
+	//   os = the stream to output the output to.
+	//   output = the vector to be printed.
+	// Return:
+	//   the stream with the value to be printed
 	std::ostream& operator<<(std::ostream& os, const Vector2D& output);
+
+	// Overloads the * operator to multiply a float by a vector
+	// Params:
+	//   other = the scalar.
+	//   vector2D = the vector to be scaled.
+	// Return:
+	//   The result of the inverse operation (i.e. Vector2D * other).
 	Vector2D operator*(float other, const Vector2D& vector2D);
 	
 
