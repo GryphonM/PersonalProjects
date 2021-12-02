9e9e37e3da9e40cebdb3f68be7b556e2df86bf61
Gryphon M <gryphon.mclaughlin@digipen.edu>
2021-12-02 13:36:10 -0800

    Comments + Zip File

--- Projects/CppProjects/CS230 Projects/Starting the Loop/Source/Level1.cpp
+++ Projects/CppProjects/CS230 Projects/Starting the Loop/Source/Level1.cpp
@@ -6,16 +6,19 @@ Level1::Level1() : BetaObject("Level1")
 {
 }
 
+// Initializes the level
 void Level1::Initialize()
 {
 	std::cout << "Level1::Initialize" << std::endl;
 }
 
+// Updates the level based on the time passed since the last interval
 void Level1::Update(float dt)
 {
 	std::cout << "Level1::Update - " << dt << std::endl;
 }
 
+// Manages the shutdown of the level
 void Level1::Shutdown()
 {
 	std::cout << "Level1::Shutdown" << std::endl;
--- Projects/CppProjects/CS230 Projects/Starting the Loop/Source/Level1.h
+++ Projects/CppProjects/CS230 Projects/Starting the Loop/Source/Level1.h
@@ -8,7 +8,10 @@ class Level1 : public Beta::BetaObject
 public:
 	Level1();
 
+	// Initializes the level
 	void Initialize();
+	// Updates the level based on the time passed since the last interval
 	void Update(float dt);
+	// Manages the shutdown of the level
 	void Shutdown();
 };
