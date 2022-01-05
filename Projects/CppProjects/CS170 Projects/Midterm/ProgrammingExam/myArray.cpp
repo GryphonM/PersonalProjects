bbef0e253a394657b2729c8eda5f2b2ed6fa20dd
Gryphon M <71658735+GryphonM@users.noreply.github.com>
2022-01-05 14:45:17 -0800

    Finish Midterm

--- Projects/CppProjects/CS170 Projects/Midterm/ProgrammingExam/myArray.cpp
+++ Projects/CppProjects/CS170 Projects/Midterm/ProgrammingExam/myArray.cpp
@@ -1,69 +1,102 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	myArray.cpp
+// Author(s):	Gryphon McLaughlin (gryphon.mclaughlin)
+// Project:	CS170 6.0.3 EXAM: Midterm (Programming Portion)
+// Course:	WANIC VGP2
+//
+// Copyright © 2019 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
 #include "MyArray.h"
 
-#ifdef _DEBUG
-#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
-// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
-// allocations to be of _CLIENT_BLOCK type
-#else
-#define DBG_NEW new
-#endif
-
+// Default Constructor for class
 myArray::myArray()
 {
     size = 0;
     capacity = 1;
-    numbers = DBG_NEW int[1];
+    numbers = new int[1];
 }
 
+// Copy Constructor for class
+// Params:
+//   other = Other object to copy from
 myArray::myArray(const myArray& other)
 {
     size = other.size;
     capacity = other.size;
 
-    numbers = DBG_NEW int[capacity];
+    // Copy values to this array
+    numbers = new int[capacity];
     for (int i = 0; i < size; i++)
         numbers[i] = other.numbers[i];
 }
 
+// Constructor with set variables.
+// Params:
+//   numbers_ = array of numbers to assign to the numbers value.
+//   size_ = how many values in the array
+//   capacity_ = how many values can be stored in the array
+myArray::myArray(int* numbers_, int size_, int capacity_)
+{
+    size = size_;
+    capacity = capacity_;
+    numbers = numbers_;
+}
+
+// Destructor for class
 myArray::~myArray()
 {
     delete[] numbers;
 }
 
+// Returns the capacity of the array
 int myArray::get_capacity(void) const
 {
     return capacity;
 }
 
+// Returns the size of the array
 int myArray::get_size(void) const
 {
     return size;
 }
 
+// Adds a value to the end of the array
+// Params:
+//   value_ = value to add to array
 void myArray::push(int value_)
 {
+    // Double capacity if full
     if (size == capacity)
     {
         capacity *= 2;
-        int* newArray = DBG_NEW int[capacity];
+        int* newArray = new int[capacity];
         for (int i = 0; i < size; i++)
             newArray[i] = numbers[i];
 
+        delete[] numbers;
         numbers = newArray;
-        delete[] newArray;
     }
 
     numbers[size] = value_;
     size++;
 }
 
+// Removes last value from array
 void myArray::pop()
 {
     if (size > 0)
         size--;
 }
 
-myArray& myArray::operator=(const myArray& other)
+// Overloads the assignment operator for this class
+// Params:
+//   other = the array to assign this arrays values to.
+// Return:
+//   A pointer to this array object
+myArray myArray::operator=(const myArray& other)
 {
     if (&other != this)
     {
@@ -72,7 +105,8 @@ myArray& myArray::operator=(const myArray& other)
         size = other.size;
         capacity = other.capacity;
 
-        numbers = DBG_NEW int[capacity];
+        // Copy values to this array
+        numbers = new int[capacity];
         for (int i = 0; i < size; i++)
             numbers[i] = other.numbers[i];
     }
@@ -80,28 +114,90 @@ myArray& myArray::operator=(const myArray& other)
     return *this;
 }
 
-myArray& myArray::operator+(const myArray& other) const
+// Overloads the + operator for this class
+// Params:
+//   other = Array to add to this class
+// Return:
+//   A pointer to this array object
+myArray myArray::operator+(const myArray& other) const
 {
-    myArray *out = DBG_NEW myArray();
+    // Determine which capacity is larger
+    int capacity_;
+    if (capacity < other.capacity)
+        capacity_ = other.capacity;
+    else
+        capacity_ = capacity;
+
+    // Allocate memory for return array
+    int* numbers_ = new int[capacity_];
 
     int count = 0;
     for (; count < size && count < other.size; count++)
-        out->push(numbers[count] + other.numbers[count]);
+        numbers_[count] = numbers[count] + other.numbers[count];
 
+    // Determine which array has leftover elements
     if (count + 1 < size)
     {
         for (; count < size; count++)
-            out->push(numbers[count]);
+            numbers_[count] = numbers[count];
     }
     else if (count + 1 < other.size)
     {
         for (; count < other.size; count++)
-            out->push(other.numbers[count]);
+            numbers_[count] = other.numbers[count];
     }
 
-    return *out;
+    return myArray(numbers_, count, count);
 }
 
+// Overloads the += operator for this class
+// Params:
+//   other = Array to add to this class
+// Return:
+//   A pointer to this array object
+myArray myArray::operator+=(const myArray& other)
+{
+    // Store values of this numbers array in temporary array
+    int* tempNumbers = new int[capacity];
+    for (int i = 0; i < size; ++i)
+        tempNumbers[i] = numbers[i];
+
+    if (capacity < other.capacity)
+        capacity = other.capacity;
+
+    // Reallocate numbers array to fit the new size
+    delete[] numbers;
+    numbers = new int[capacity];
+
+    int count = 0;
+    for (; count < size && count < other.size; count++)
+        numbers[count] = tempNumbers[count] + other.numbers[count];
+
+    // Determine which array has leftover elements
+    if (count + 1 < size)
+    {
+        for (; count < size; count++)
+            numbers[count] = tempNumbers[count];
+    }
+    else if (count + 1 < other.size)
+    {
+        for (; count < other.size; count++)
+            numbers[count] = other.numbers[count];
+    }
+
+    // Clear allocated array and set the size
+    delete[] tempNumbers;
+    size = count;
+
+    return *this;
+}
+
+// Overloads the << operator for this class
+// Params:
+//   os = stream to output the array to
+//   a_ = array to output
+// Return:
+//   The stream where the array was output
 std::ostream& operator<<(std::ostream& os, const myArray& a_)
 {
     for (int i = 0; i < a_.size; i++)
--- Projects/CppProjects/CS170 Projects/Midterm/ProgrammingExam/myArray.h
+++ Projects/CppProjects/CS170 Projects/Midterm/ProgrammingExam/myArray.h
@@ -1,20 +1,81 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	myArray.h
+// Author(s):	Gryphon McLaughlin (gryphon.mclaughlin)
+// Project:	CS170 6.0.3 EXAM: Midterm (Programming Portion)
+// Course:	WANIC VGP2
+//
+// Copyright © 2019 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
 #pragma once
 #include <iostream>
 
 class myArray
 {
 public:
+    // Default Constructor for class
     myArray(void);
+
+    // Copy Constructor for class
+    // Params:
+    //   other = Other object to copy from
     myArray(const myArray& other);
+
+    // Constructor with set variables.
+    // Params:
+    //   numbers_ = array of numbers to assign to the numbers value.
+    //   size_ = how many values in the array
+    //   capacity_ = how many values can be stored in the array
+    myArray(int* numbers_, int size_, int capacity_);
+
+    // Destructor for class
     ~myArray(void);
 
-    int get_capacity(void) const;
-    int get_size(void) const;
-    void push(int value_);
-    void pop();
-    myArray& operator=(const myArray& other);
-    myArray& operator+(const myArray& other) const;
 
+    // Returns the capacity of the array
+    int get_capacity(void) const;
+
+    // Returns the size of the array
+    int get_size(void) const;
+
+    // Adds a value to the end of the array
+    // Params:
+    //   value_ = value to add to array
+    void push(int value_);
+
+    // Removes last value from array
+    void pop();
+
+    // Overloads the assignment operator for this class
+    // Params:
+    //   other = the array to assign this arrays values to.
+    // Return:
+    //   A pointer to this array object
+    myArray operator=(const myArray& other);
+
+    // Overloads the + operator for this class
+    // Params:
+    //   other = Array to add to this class
+    // Return:
+    //   A pointer to this array object
+    myArray operator+(const myArray& other) const;
+
+    // Overloads the += operator for this class
+    // Params:
+    //   other = Array to add to this class
+    // Return:
+    //   A pointer to this array object
+    myArray operator+=(const myArray& other);
+
+
+    // Overloads the << operator for this class
+    // Params:
+    //   os = stream to output the array to
+    //   a_ = array to output
+    // Return:
+    //   The stream where the array was output
     friend std::ostream& operator<<(std::ostream& os, const myArray& a_);
 
 private:
